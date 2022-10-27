41e6144f1e4d30bbc34859186bc02ac361129c90
Cornel <cornelcodes@gmail.com>
2022-10-27 16:43:25 +0200

    added ideaboard structure and ui changes

---
+++ WebAppTemplate/Controllers/IdeaboardController.cs
@@ -0,0 +1,21 @@
+﻿using DAL.BOMContext;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BusinessOverviewManagerClient.Controllers
+{
+    [Route("[controller]")]
+    public class IdeaboardController : Controller
+    {
+        private bomContext db;
+        public IdeaboardController(bomContext _db)
+        {
+            db = _db;
+        }
+
+        [HttpGet("")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
