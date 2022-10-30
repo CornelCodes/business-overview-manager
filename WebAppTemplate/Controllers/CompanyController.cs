92da0ccf9c1d672432f02195bfdc081244fc4917
Cornel <cornelcodes@gmail.com>
2022-10-30 12:56:49 +0200

    company create and edit

--- WebAppTemplate/Controllers/CompanyController.cs
+++ WebAppTemplate/Controllers/CompanyController.cs
@@ -1,21 +1,72 @@
 ﻿using DAL.BOMContext;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Conventions;
 
 namespace BusinessOverviewManagerClient.Controllers
 {
     [Route("[controller]")]
     public class CompanyController : Controller
     {
-        private bomContext db;
+        private bomContext mDb;
         public CompanyController(bomContext _db)
         {
-            db = _db;
+            mDb = _db;
         }
 
         [HttpGet("")]
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
+            var userId = DAL.Helpers.AuthenticationHelper.GetUserId(HttpContext);
+            var user = await mDb.Users.Include(x => x.Companies.OrderBy(y => y.Id)).FirstOrDefaultAsync(x => x.Id == Int64.Parse(userId));
+
+            return View(user);
+        }
+
+        [HttpGet("[action]/{companyId}")]
+        public async Task<IActionResult> Details(long companyId)
+        {
+            var company = await mDb.Companies.FirstOrDefaultAsync(x => x.Id == companyId);
+            if (company == null)
+            {
+                company = new Company();
+            }
+            return View(company);
+        }
+
+        [HttpPost("[action]")]
+        public async Task<IActionResult> Update(Company vm)
+        {
+            try
+            {
+                var userId = Int64.Parse(DAL.Helpers.AuthenticationHelper.GetUserId(HttpContext));
+                var company = await mDb.Companies.FirstOrDefaultAsync(x => x.Id == vm.Id);
+
+                if (company != null)
+                {
+                    //update properties
+                    company.Name = vm.Name;
+                }
+                else
+                {
+                    //create new
+                    company = new Company()
+                    {
+                        Name = vm.Name,
+                        UserId = userId
+                    };
+                    await mDb.Companies.AddAsync(company);
+                }
+
+                await mDb.SaveChangesAsync();
+                vm = company;
+                return RedirectToAction("Index");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
         }
     }
 }
